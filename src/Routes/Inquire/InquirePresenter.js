ba70513af2f396b40dc47413f2f156d4bab8f1c5
sun00131 <seonyeong0131@gmail.com>
2022-06-05 15:58:23 +0900

    update : 교육문의 페이지 버튼 추가
    onclick 안 되는 거 수정

--- src/Routes/Inquire/InquirePresenter.js
+++ src/Routes/Inquire/InquirePresenter.js
@@ -1,13 +1,14 @@
 import React from "react";
 import SubTop2 from "Components/SubTop2";
-
+import Buttons from "Components/Buttons";
 
 const InquirePresenter = ({
   subTopData,
 }) => (
   <>
     <SubTop2 subTopData={subTopData} />
-    
+    <Buttons title="교육 문의하기"/>
+    <Buttons title="카카오톡으로 교육 문의, 신청하기"/>
   </>
 );
 
